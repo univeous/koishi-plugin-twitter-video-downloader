68f7de84782bdcee6959bc9c7f1b84b47d5113e7
Univeous <univeous@gmail.com>
2022-08-23 10:21:49 +0800

    initial commit

---
+++ readme.md
@@ -0,0 +1,20 @@
+# koishi-plugin-twitter-video-downloader
+
+[![npm](https://img.shields.io/npm/v/koishi-plugin-twitter-video-downloader?style=flat-square)](https://www.npmjs.com/package/koishi-plugin-twitter-video-downloader)
+
+Twitter视频下载插件 For [Koishi](https://github.com/koishijs/koishi) v4。
+
+请确保你的bot运行在一个适当的网络环境下。
+
+## 配置项
+
+### parseUrl
+
+- 类型: `boolean`
+- 默认值: `true`
+
+是否解析Twitter链接。如果为否，则只能通过命令调用。
+
+## 指令：twvd
+
+- 基本语法：`twvd <url>`
---
+++ src/index.ts
@@ -0,0 +1,54 @@
+import {Context, Schema, segment} from 'koishi'
+
+export const name = 'twitter-video-downloader'
+
+const urlValidation = /http(?:s)?:\/\/(?:www\.)?twitter\.com\/([a-zA-Z0-9_]+)/
+const ApiEndpoint = 'https://mp3downy.com/Home/StartProgressTwitterDownload'
+
+export interface Config {
+  //showWarning?: boolean
+  parseUrl?: boolean
+}
+
+export const Config: Schema<Config> = Schema.object({
+  //showWarning: Schema.boolean().default(false).description('解析失败时是否发送提示。'),
+  parseUrl: Schema.boolean().default(true).description('是否解析Twitter链接。如果为否，则只能通过命令调用。')
+})
+
+//let dispose: () => boolean | void
+
+export function apply(ctx: Context, config: Config) {
+  const {parseUrl} = config
+
+  //if(parseUrl){
+  ctx.middleware(async (session, next) => {
+    if (!parseUrl) return next()
+    if (!session.content.match(urlValidation)) return next()
+    const result = await handleUrl(session.content)
+    if (result === 'Error (or) no supported video found.' || result.result.VideosInfo.length === 0) return next()
+    const {VideosInfo} = result.result
+    await session.sendQueued(segment.video(VideosInfo[VideosInfo.length - 1].Url))
+    return next()
+  })
+  //} else if (dispose) dispose();
+
+  ctx.command('twvd <url:text>', 'Twitter视频下载')
+    .action(async ({session}, url) => {
+      if (!parseUrl) return '请提供url。'
+      const match = url.match(urlValidation)
+      if (!match) return '无效的url。'
+      const result = await handleUrl(url)
+      if (result === 'Error (or) no supported video found.') return '无效的url。'
+      if (result.result.VideosInfo.length === 0) return '未找到视频。'
+      const {VideosInfo} = result.result
+      return segment.video(VideosInfo[VideosInfo.length - 1].Url)
+    })
+
+  function handleUrl(url: string) {
+    return ctx.http.post(ApiEndpoint, {
+      Quantity: 'MP4',
+      Type: 'MP4',
+      Url: url
+    })
+  }
+}
