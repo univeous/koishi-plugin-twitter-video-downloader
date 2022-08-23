ede434591ee3986925518c5bafd0cac44c43af71
Univeous <univeous@gmail.com>
2022-08-23 10:52:28 +0800

    bug fix

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ import {Context, Schema, segment} from 'koishi'
 
 export const name = 'twitter-video-downloader'
 
-const urlValidation = /http(?:s)?:\/\/(?:www\.)?twitter\.com\/([a-zA-Z0-9_]+)/
+//const urlValidation = /http(?:s)?:\/\/(?:www\.)?twitter\.com\/([a-zA-Z0-9_]+)/
 const ApiEndpoint = 'https://mp3downy.com/Home/StartProgressTwitterDownload'
 
 export interface Config {
@@ -23,7 +23,7 @@ export function apply(ctx: Context, config: Config) {
   //if(parseUrl){
   ctx.middleware(async (session, next) => {
     if (!parseUrl) return next()
-    if (!session.content.match(urlValidation)) return next()
+    if (!session.content.includes('twitter.com')) return next()
     const result = await handleUrl(session.content)
     if (result === 'Error (or) no supported video found.' || result.result.VideosInfo.length === 0) return next()
     const {VideosInfo} = result.result
@@ -35,8 +35,8 @@ export function apply(ctx: Context, config: Config) {
   ctx.command('twvd <url:text>', 'Twitter视频下载')
     .action(async ({session}, url) => {
       if (!url) return '请提供url。'
-      const match = url.match(urlValidation)
-      if (!match) return '无效的url。'
+      //const match = url.match(urlValidation)
+      if (url.includes('twitter.com')) return '无效的url。'
       const result = await handleUrl(url)
       if (result === 'Error (or) no supported video found.') return '无效的url。'
       if (result.result.VideosInfo.length === 0) return '未找到视频。'
