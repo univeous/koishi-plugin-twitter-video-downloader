b34a573e8156f93a81fb37fedc02a849ebcc6b42
Univeous <univeous@gmail.com>
2022-08-23 11:00:42 +0800

    add options to send video url

--- src/index.ts
+++ src/index.ts
@@ -8,17 +8,19 @@ const ApiEndpoint = 'https://mp3downy.com/Home/StartProgressTwitterDownload'
 export interface Config {
   //showWarning?: boolean
   parseUrl?: boolean
+  showVideoUrl?: boolean
 }
 
 export const Config: Schema<Config> = Schema.object({
   //showWarning: Schema.boolean().default(false).description('解析失败时是否发送提示。'),
-  parseUrl: Schema.boolean().default(true).description('是否解析Twitter链接。如果为否，则只能通过命令调用。')
+  parseUrl: Schema.boolean().default(true).description('是否解析Twitter链接。如果为否，则只能通过命令调用。'),
+  showVideoUrl: Schema.boolean().default(true).description('是否发送视频链接。'),
 })
 
 //let dispose: () => boolean | void
 
 export function apply(ctx: Context, config: Config) {
-  const {parseUrl} = config
+  const {parseUrl, showVideoUrl} = config
 
   //if(parseUrl){
   ctx.middleware(async (session, next) => {
@@ -27,6 +29,7 @@ export function apply(ctx: Context, config: Config) {
     const result = await handleUrl(session.content)
     if (result === 'Error (or) no supported video found.' || result.result.VideosInfo.length === 0) return next()
     const {VideosInfo} = result.result
+    if(showVideoUrl) await session.sendQueued(VideosInfo[VideosInfo.length - 1].Url)
     await session.sendQueued(segment.video(VideosInfo[VideosInfo.length - 1].Url))
     return next()
   })
@@ -41,7 +44,9 @@ export function apply(ctx: Context, config: Config) {
       if (result === 'Error (or) no supported video found.') return '无效的url。'
       if (result.result.VideosInfo.length === 0) return '未找到视频。'
       const {VideosInfo} = result.result
-      return segment.video(VideosInfo[VideosInfo.length - 1].Url)
+      const bestQualityVideo = VideosInfo[VideosInfo.length - 1]
+      if(showVideoUrl) await session.sendQueued(bestQualityVideo.Url)
+      return segment.video(bestQualityVideo.Url)
     })
 
   function handleUrl(url: string) {
